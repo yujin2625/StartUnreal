13401f5d586e28a1542dd91c4d6adb7b4977b6cb
yujin <yujin2625@gmail.com>
2023-09-26 16:17:28 +0900

    input

---
+++ Source/StartUnreal/StartUnreal.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "StartUnreal.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, StartUnreal, "StartUnreal" );
